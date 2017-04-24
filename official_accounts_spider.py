75e80bf8fadd7d520ee2d4946640f22b44ce10c4
alihanniba <alihanniba@gmail.com>
2017-04-24 23:38:51 +0800

    爬虫调试测试中，成功1400条数据

--- official_accounts_spider.py
+++ official_accounts_spider.py
@@ -12,6 +12,9 @@ import http.cookiejar
 import pymysql  
 import types  
 import json
+import time
+import uuid
+import os
 
 class OfficialAccounts:
     def __init__(self, url=False):
@@ -32,6 +35,9 @@ class OfficialAccounts:
 
         self.official_accounts_array = []
 
+        self.base_url = os.path.split(os.path.realpath(__file__))[0]
+        self.qrcode_url = self.base_url + '/images/qrcode/'
+        self.cover_url = self.base_url + '/images/cover/'
         #连接数据库执行数据库操作
         self.db=pymysql.connect(host="localhost",user="alihanniba",charset="utf8",password="77558@Mysql",db="wxpn");  
         self.cursor=self.db.cursor() 
@@ -39,8 +45,8 @@ class OfficialAccounts:
     def get_index_page(self):
         request = urllib.request.Request(self._url, headers = self._headers)
         response = self.opener.open(request)
-        content = response.read().decode('UTF-8')
-        # content = response.read()
+        # content = response.read().decode('UTF-8')
+        content = response.read()
         # 保存cookie到cookie.txt中,  ### 其实不用
         self.cookie.save(ignore_discard=True, ignore_expires=True)  
         #以html解析格式打开
@@ -59,6 +65,7 @@ class OfficialAccounts:
             category_id = last_id[0]
             # self.db.close()
             # self.cursor.close()
+            print('----------------     抓取一级分类  '+ tag_a[0].string +'  开始    --------------------------')
             for index, content in enumerate(tag_a):
                 if index > 0: 
                     sub_database_data = (content.string, int(category_id))
@@ -70,10 +77,13 @@ class OfficialAccounts:
                     # self.db.close()
                     # self.cursor.close()
                     subcategory_url = self._url + content['href']
+                    print('----------------     抓取二级分类  '+ content.string +'  开始    --------------------------')
                     self.get_subcategories(subcategory_url, subcategory_id)
-                    return
+                    print('----------------     抓取二级分类  '+ content.string +'  结束    --------------------------')
+                    # return
                 # print(subcategory_url)
-
+            print('----------------     抓取一级分类  '+ tag_a[0].string +'  结束    --------------------------')
+            
     def get_subcategories(self, url, parent_id):
         request = urllib.request.Request(url, headers = self._headers)
         response = self.opener.open(request)
@@ -86,18 +96,15 @@ class OfficialAccounts:
         sub_nav_ul = soup.find_all("div", class_ = "newslist")
         catalog = sub_nav_ul[0].find_all("dt")
 
-        #获取页码数据，抓取下一页
-        sub_page_div = soup.find_all("div", class_ = "page")
-        #当前页码数
-        current = sub_page_div[0].find_all('span', class_ = "current")[0].string
-        
-        #最后一个a标签
-        tag_a_array = sub_page_div[0].find_all('a')
-        
-        tag_a_last = tag_a_array[len(tag_a_array) - 1].string
-        print(tag_a_last)
-        return
-        
+        try:
+            #获取页码数据，抓取下一页
+            sub_page_div = soup.find_all("div", class_ = "page")
+            #当前页码数
+            current = sub_page_div[0].find_all('span', class_ = "current")[0].string
+        except IndexError:
+            sub_page_div = []
+            current = '0'
+       
         #重置数据组装
         self.official_accounts_array = []
         
@@ -107,14 +114,29 @@ class OfficialAccounts:
             self.get_thirdcategories(thirdcategory_url, parent_id)
         self.cursor.executemany('INSERT INTO official_accounts_thirdcategories (name, wechat_id, attention_count, address, include_time, parent_id, descriptions, qrcode, wechat_link, background_cover) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', self.official_accounts_array)
         self.db.commit() 
-        print('----------第一页插库进行中...----------------')
-        self.db.close()
-        self.cursor.close()
-        print('----------第一页插库成功----------------')
+        print('----------第   '+ current +'   页插库进行中...----------------')
+        
+        print('----------第   '+ current +'   页插库成功---------------------')
         #抓取完一页后睡眠一秒
-        sleep(1000)
+        print('-----------------     休眠一秒     --------------------------')
+        time.sleep(1)
 
-        return
+        #最后一个a标签
+        try:
+            tag_a_array = sub_page_div[0].find_all('a')
+            tag_a_last = tag_a_array[len(tag_a_array) - 1].string
+            tag_a_link = tag_a_array[len(tag_a_array) - 1]['href']
+        except IndexError:
+            tag_a_last = ''
+            tag_a_link = ''
+        
+        if tag_a_last == '下一页':
+            next_url = self._url + tag_a_link
+            self.get_subcategories(next_url, parent_id)
+        
+        # self.db.close()
+        # self.cursor.close()
+        # return
     
     def get_thirdcategories(self, url, parent_id):
         request = urllib.request.Request(url, headers = self._headers)
@@ -132,7 +154,7 @@ class OfficialAccounts:
         #获取公众号二维码
         try:
             content_img = sub_nav_ul[0].find_all("div", class_ = "qucode_img")
-            tag_img = content_img[0].find_all("img")
+            tag_img = content_img[0].find_all("img") or ''
         except SyntaxError:
             pass
 
@@ -158,7 +180,7 @@ class OfficialAccounts:
         #获取关注度
         try:
             attention = re.findall(r"(\d+)", str(tag_lis[2]))
-            attention_count = attention[0]
+            attention_count = attention[0] or ''
         except SyntaxError:
             attention_count = 0
 
@@ -166,27 +188,33 @@ class OfficialAccounts:
         try:
             chinese_word = re.compile(u"[\u4e00-\u9fa5]+") 
             address_array = re.findall(chinese_word, str(tag_lis[4]))
-            address = address_array[1]
+            address = address_array[1] or ''
         except SyntaxError:
             address = ''
         
 
         #获取收录时间
         try:
-            include_time_array = re.findall(r"(\d+)", str(tag_lis[5]))
+            include_time_array = re.findall(r"(\d+)", str(tag_lis[5])) or ''
             include_time = include_time_array[0] + '-' + include_time_array[1] + '-' + include_time_array[2]
         except SyntaxError:
             include_time = ''
         
         #获取二维码
         try:
-            qrcode = tag_img[0]['src']
+            qrcode_link = tag_img[0]['src']
+            random_str_q = uuid.uuid5(uuid.NAMESPACE_DNS, str(uuid.uuid1()))
+            qrcode = str(self.qrcode_url) + str(random_str_q) + '.jpg'
+            qr = requests.get(qrcode_link)
+            with open(qrcode, 'wb') as outfile:
+                outfile.write(qr.content)
+            # urllib.request.urlretrieve(qrcode_link)
         except SyntaxError:
             qrcode = ''
 
         #获取微信名
         try:
-            name = tag_h1[0].string
+            name = tag_h1[0].string or ''
         except SyntaxError:
             name = ''
 
@@ -199,22 +227,32 @@ class OfficialAccounts:
         
         #获取背景头像
         try:
-            background_cover = sub_description_div[0].find_all('img')[0]['src']
+            background_cover_link = sub_description_div[0].find_all('img')[0]['src']
+            random_str_b = uuid.uuid5(uuid.NAMESPACE_DNS, str(uuid.uuid1()))
+            background_cover = str(self.cover_url) + str(random_str_b) + '.jpg'
+            br = requests.get(background_cover_link)
+            with open(background_cover, 'wb') as outfile:
+                outfile.write(br.content)
         except SyntaxError:
             background_cover = ''
 
         #获取介绍
         try:
-            descriptions = sub_description_div[1].string
+            descriptions = sub_description_div[1].string or ''
         except SyntaxError:
             descriptions = ''
         
         #获取微信公众号链接
         try:
             wechat_link = sub_description_div[2].find_all('a')[0]['href'] or ''
-            wechat_link = re.search('mp.weixin.qq.com', wechat_link) or ''
+            wechat_link_mp = re.search('mp.weixin.qq.com', wechat_link)
+            if not wechat_link_mp:
+                wechat_link = ''
+            else:
+                wechat_link = wechat_link
         except IndexError:
             wechat_link = ''
+
         print('微信名--------------' + name)
         print('微信号--------------' + wechat_id)
         print('关注度--------------' + attention_count)
@@ -229,8 +267,6 @@ class OfficialAccounts:
         one_account_data = (name, wechat_id, attention_count, address, include_time, parent_id, descriptions, qrcode, wechat_link, background_cover)
         self.official_accounts_array.append(one_account_data)
         print('------------------'+ name +'  组装完成   ------------------------')
-        
-        
 
 official_accounts = OfficialAccounts()
 official_accounts.get_index_page()
